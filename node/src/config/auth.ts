bd442e3f06da901c383f22a6ff2c0eb6cf5189c9
eMatheus23 <eliasmatheus@hotmail.com>
2020-12-25 17:04:03 -0300

    Finish auth implementation

---
+++ node/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'b6fdc484cb98757f70049c15007492a0',
+    expiresIn: '1d',
+  },
+};
--- node/src/routes/appointments.routes.ts
+++ node/src/routes/appointments.routes.ts
@@ -5,7 +5,11 @@ import { getCustomRepository } from 'typeorm';
 import AppointmentRepository from '../repositories/AppointmentRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
+import ensureAuthenticated from '../middlewares/ensureAthenticated';
+
 const appointmentsRouter = Router();
+
+appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.get('/', async (request, response) => {
   const appointmentsRepository = getCustomRepository(AppointmentRepository);
