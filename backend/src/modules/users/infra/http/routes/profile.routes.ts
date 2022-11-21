63e9196b2641696140581b1ebdc8c3afcbc8784b
Elias Matheus Oliveira <eliasmatheus@hotmail.com>
2022-11-20 22:50:03 -0300

    [10] Add schema validation with celebrate

--- backend/src/modules/users/infra/http/routes/profile.routes.ts
+++ backend/src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProfileController from '../controllers/ProfileController';
@@ -8,7 +9,20 @@ const profileController = new ProfileController();
 
 profileRouter.use(ensureAuthenticated);
 
-profileRouter.get('/', profileController.show);
+profileRouter.get(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      id: Joi.string().uuid().required(),
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      old_password: Joi.string(),
+      password: Joi.string(),
+      password_confirmation: Joi.string().valid(Joi.ref('password')),
+    },
+  }),
+  profileController.show,
+);
 profileRouter.put('/', profileController.update);
 
 export default profileRouter;
