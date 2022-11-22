749fecddbe02fdb5cdb6c201f1bd97f4cbec7f8a
Elias Matheus Oliveira <eliasmatheus@hotmail.com>
2022-11-22 12:16:03 -0300

    [10] Implement email sending with AWS SES

---
+++ backend/src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'eliasmatheus@hotmail.com',
+      name: 'Elias Oliveira',
+    },
+  },
+} as IMailConfig;
